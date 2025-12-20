f9758e805fe7d0c9388b5867f3f3f7d198dc73a1
اسمك <@.com>
2025-12-20 15:53:05 +0200

    Deployment on railway

---
+++ .cmd
@@ -0,0 +1,2 @@
+start cmd.exe /c <php artisan serve>
+start cmd.exe /c <cd madrid-app & npm start>
---
+++ start.bat
@@ -0,0 +1,2 @@
+start cmd.exe /c php artisan serve
+start cmd.exe /k "cd /d c:\xampp\htdocs\MadridTours\madrid-app && npm start"
